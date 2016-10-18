14146a9d6068dd90b258369fbd3c5fb85f9a85b1
MUTEBA Jean <HE201324@students.ephec.be>
2016-10-18 09:49:17 +0200

    v0.0.1

---
+++ Projet-course-F1-v-2.c
@@ -0,0 +1,80 @@
+#include <rand.h>		//Génération de nombres aléatoires
+#include <vector>
+#include <stdio.h>
+#include <std.lib>
+#include <stdbool.h>	//Librairie des booléens
+#include <time.h>		//Simule les temps des coureurs
+
+
+
+
+struct voiture
+	{
+		int numero;
+		bool abandon;
+		bool pitStop;
+		bool enPanne;
+		double tempsEssais[3];
+		double tempsQualifs[3];
+		double tempsTourEssais;
+		double tempsTourQualifs;
+	};
+	double s1;
+	
+int main(int argc,char **argv)
+{
+	
+	//Vendredi matin : essai libre 1 "heure" 30 (P1) checker si abandon et panne beforehand
+	
+	
+	
+			//Relevé temps S1
+			
+			
+			if(enPanne || abandon)
+			{
+				exit(failure);
+				
+			}else
+			{
+					//random du temps , tempsEssais[0]
+					s1 = srand(a,b,int a; int b)
+					{
+						
+						
+					};
+			}
+			
+			//Relevé temps S2
+			
+			//Relevé temps S3
+	
+	//Vendredi aprèm : essai libre 1 "heure" 30  (P2)
+	
+			//Relevé temps S1
+			
+			//Relevé temps S2
+			
+			//Relevé temps S3
+			
+	//Samedi matin : essai libre 1 "heure" 30 (P3)
+			
+			//Relevé temps S1
+			
+			//Relevé temps S2
+			
+			//Relevé temps S3
+			
+	//Samedi aprèm
+	
+			//Q1, durée : 18 minutes, six joueurs en moins à la fin (retrait des éléments 17 à 22)
+			
+			//Q2, durée : 15 minutes, six joueurs en moins à la fin (retrait des éléments 16 à 11)
+			
+			//Q3, durée : 12 minutes, classement des joueurs pour décider de leur place à la dernière course
+			
+	//Dimanche après-midi : dernière course & classement final	avec tous les coureurs dans l'ordre défini par les séances libres.
+	
+	
+	
+}
