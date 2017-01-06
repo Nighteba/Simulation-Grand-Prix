b77a81aa7893a124f6a4c3e2ede760dd869b1cb5
bastiaens <olivier.bastiaens@gmail.com>
2017-01-06 19:51:45 +0100

    le grp final

--- grp.c
+++ grp.c
@@ -6,110 +6,86 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/types.h>
-#include<sys/ipc.h>
+#include <sys/ipc.h>
 #include <sys/sem.h>
 #include <errno.h>
+#include "proto.h"
 #define NB_TEMPS 3
-#define NB_COUREURS 3
-
-
-typedef struct voiture voiture;
-struct voiture {
-        int numero;
-        int abandon;
-        bool pitStop;
-        unsigned int tpsSecteur[NB_TEMPS];
-        unsigned int tpsTour;
-};
-
+#define NB_COUREURS 22
 
 int main(int argc, char*argv[]){
-        int i;
-        int j;
-        int k;
-        int l;
+        int i, j;       // variables d'iterations dans les boucles
+        int min = 40000;
+        int max = 60000;
         pid_t pid;
-        struct voiture course[NB_COUREURS];
-        int classement seance[3];                      //retient le classement de P1,P2 & P3.
+        voiture course[NB_COUREURS];
+        course[0].numero = 44;
+        course[1].numero = 6;
+        course[2].numero = 5;
+        course[3].numero = 7;
+        course[4].numero = 3;
+        course[5].numero = 33;
+        course[6].numero = 19;
+        course[7].numero = 77;
+        course[8].numero = 11;
+        course[9].numero = 27;
+        course[10].numero = 26;
+        course[11].numero = 55;
+        course[12].numero = 14;
+        course[13].numero = 22;
+        course[14].numero = 9;
+		course[15].numero = 12;
+        course[16].numero = 20;
+        course[17].numero = 30;
+        course[18].numero = 8;
+        course[19].numero = 21;
+        course[20].numero = 31;
+        course[21].numero = 94;
 
-int main(int argc, char*argv[]){
-        int i;
-        int j;
-        int k;
-        int l;
-        pid_t pid;
-        struct voiture course[NB_COUREURS];             //ensemble des competiteurs.
-        size_t size_k = sizeof(voiture);
-        key_t key = 9999 ; /* clé à passer a shmget*/
+		size_t size_k = sizeof(voiture);
+        key_t key=8888 ; /* clé à passer a shmget*/
         int shmFlag; /*flag  à passer a shmget*/
         int shmid; /*l'id de la memoire partagée*/
-        char * pmem; // pointeur vers la memoire partagée
+        voiture *pmem; // pointeur vers la memoire partagée
 
-        shmid = shmget (key, size_k*NB_COUREURS,IPC_CREAT|0666); // crée la mémoire partagée, la clé, sa taille, les permissions
+        shmid = shmget (key, size_k*NB_COUREURS,IPC_CREAT|0666);// crée la mémoire partagée, la clé, sa taille, les permission
+        pmem = shmat(shmid, NULL, 0);
 
-        if(shmid== -1){          
+        if(shmid== -1){
                 perror("shmget fail");
                 exit(1);
         }
 
-
-        srand(time(0));
         for(i = 0; i < NB_COUREURS; i++){
                 pid = fork();
-                pmem = shmat(shmid,0,0); // attache un segment aux processus courant
+
                 if(pid == -1){
                         exit(EXIT_FAILURE);
                 }
- 				if(pid = 0){            //processus fils, le coureur.
-                        printf("voiture num:%d", j);
+                if(pid == 0){
+                        //printf("voiture num:%d", j);
                         voiture v;
                         for(j = 0; j < NB_TEMPS; j++){
-                                v.abandon = (rand() % 10);
-
-                                if(v.abandon % 10 != 0){
-                                        v.tpsSecteur[j] =(rand() % 10);
+                                v.abandon = randoms(1, 10);
+                                if(v.abandon != 5 ){
+                                        v.tpsSecteur[j] = randoms(40000, 60000);
                                 }else{
 
                                         v.tpsSecteur[j] = 0;
+                                        return 0;
                                 }
-                                //printf("Temps secteur: %u\n", v.tpsSecteur[i]);
-                                v.tpsTour += v.tpsSecteur[i];
+								 //printf("Temps secteur: %u\n", v.tpsSecteur[i]);
+                                v.tpsTour += v.tpsSecteur[j];
                                 //printf("%u\n", v.tpsTour);
+                        }
 
-                  }
+                 shmdt(pmem); //détache le segment de la mémoire partagée                       
+                }//fin code du fils
 
+        return 0;
+        }//fin du for  de temps Secteur
 
-            }
- }
-
-
-        for(k = 0; k < NB_COUREURS; k++){
-        printf("Voiture : %d\n", k);
-                for(l = 0; l < 3; l++){
-                printf("Temps secteur : %d\n", l);
-                }
-        }
-
-        shmdt(pmem); //détache le segment de la mémoire partagée
-
-}//main
-        
-        // To Do list :
-        
-        // 1) Generer des temps coherents allant de 40000 ms à 60000 ms. (utiliser des constantes?)
-        // 2) Semaphores
-        // (-) Periodes d'essais libres
-        
-        // 3) Gerer les essais libres P1,P2 & P3
-        // 4) Conserver tous ces temps après chaque séance.
-        
-        // (-) Qualifications : 
-        // Idem que les essais libres, mais apres chaque Qn, les pires voitures sont out.
-                //Fin Q1 : 22 - 6 = 16 coureurs en lice
-                //Fin Q2 : 16 - 6 = 10 coureurs en lice
-                //Fin Q3 : Places de 1  à 10 de la grille de départ
-        //Ordre final pour la course finale
-        
+}//fin de la main               
         
 
 
